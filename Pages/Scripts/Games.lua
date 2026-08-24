e2e3765f4ce6bbcd8f6b04d4572b69795ffe629a
jezz-lab <jezzby408@gmail.com>
2026-08-25 04:28:36 +0800

    Add Games.lua with game script mappings

---
+++ Pages/Scripts/Games.lua
@@ -0,0 +1,22 @@
+--==================================================
+-- ARETEON | Pages/Scripts/Games.lua
+--==================================================
+
+return {
+
+    [2753915549] = {
+        Name = "Blox Fruits",
+        Script = "BloxFruits.lua"
+    },
+
+    [286090429] = {
+        Name = "Arsenal",
+        Script = "Arsenal.lua"
+    },
+
+    [2788229376] = {
+        Name = "Da Hood",
+        Script = "DaHood.lua"
+    },
+
+}
